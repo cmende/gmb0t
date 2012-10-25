6a43f8dfa7ba5cc3f9f5f82cc8434f42659baa96
Christoph Mende <mende.christoph@gmail.com>
2012-10-26 00:00:00 +0200

    Load Monopoly

--- gmb0t.go
+++ gmb0t.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/cmende/gmb0t/bot"
+	"github.com/cmende/gmb0t/games/monopoly"
 	"math/rand"
 	"time"
 )
@@ -9,6 +10,9 @@ import (
 func main() {
 	// we don't need a secure random
 	rand.Seed(time.Now().UnixNano())
+
+	// load games
+	bot.Games = append(bot.Games, bot.GameInfo{Name: "Monopoly", StartCallback: monopoly.StartGame, ActionCallback: monopoly.Parse})
 
 	bot.Connect()
 	<-bot.Quit
