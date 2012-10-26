cfebb11808fb8c5c631b22eb7d98c448fb91ddd9
Christoph Mende <mende.christoph@gmail.com>
2012-10-26 15:10:39 +0200

    monopoly: Implement nextPlayer()

--- games/monopoly/monopoly.go
+++ games/monopoly/monopoly.go
@@ -12,19 +12,36 @@ type Player struct {
 }
 
 var players []Player
+var currentPlayer int = 0
 
 func StartGame(nicks []string) {
 	for _, nick := range nicks {
 		fmt.Printf("New player: %s\n", nick)
 		players = append(players, Player{nick: nick, money: 1500, position: 0})
 	}
-	roll1, roll2 := roll()
-	bot.Say(fmt.Sprintf("%d/%d", roll1, roll2))
+	nextPlayer()
+}
+
+func nextPlayer() {
+	player := &players[currentPlayer]
+	result := roll()
+	player.position += result
+	bot.Say(fmt.Sprintf("%s, it's your turn. You rolled %d. New position: %d", player.nick, result, player.position))
+
+	currentPlayer++
+	if currentPlayer >= len(players) {
+		currentPlayer = 0
+	}
 }
 
 func Parse(sender, action string) {
+	switch action {
+	case "done":
+		nextPlayer()
+	default:
+	}
 }
 
-func roll() (int, int) {
-	return rand.Intn(6) + 1, rand.Intn(6) + 1
+func roll() int {
+	return rand.Intn(6) + 1 + rand.Intn(6) + 1
 }
